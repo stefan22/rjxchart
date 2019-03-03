d3fcc22ec0b641fe7de42826747344adc4b99b75
Stefano <stefan.emailacct@gmail.com>
2019-03-03 13:53:23 +0000

    [topnavigation]top navigation component

--- src/components/header/TopNavigation.js
+++ src/components/header/TopNavigation.js
@@ -3,8 +3,6 @@ import {NavLink} from 'react-router-dom';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 import '../../scss/header.scss';
 
 
