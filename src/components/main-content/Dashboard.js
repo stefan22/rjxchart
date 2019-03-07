d6e4b267a4b490eeb6a4325f0fff531fecccd981
Stefano <stefan.emailacct@gmail.com>
2019-03-07 19:53:45 +0000

    [tests]dashboard component

--- src/components/main-content/Dashboard.js
+++ src/components/main-content/Dashboard.js
@@ -63,7 +63,6 @@ class Dashboard extends Component {
               </div>
             </section>
 
-
           </div>
         }
 
--- src/tests/components/main-content/Dashboard.test.js
+++ src/tests/components/main-content/Dashboard.test.js
@@ -1,121 +1,123 @@
 import React from 'react';
-import {shallow, mount} from 'enzyme';
+import {shallow, mount, render} from 'enzyme';
 import Dashboard from '../../../components/main-content/Dashboard';
 import toJSON from 'enzyme-to-json';
 
 
+
 describe('Dashboard component', () => {
-  describe('isLoading set to false', () => {
-    beforeEach(() => {
-      const wrapper = shallow(<Dashboard />);
-      wrapper.setState({
-        isLoading: false,
-      });
-    });
-    it('should render dashboard page correctly', () => {
-      const wrapper = shallow(<Dashboard />);
-      expect(toJSON(wrapper)).toMatchSnapshot();
-    });
-    it('should have class of "mid-section-wrapper"', () => {
-      const wrapper = shallow(<Dashboard />);
-      expect(wrapper.find('.mid-section-wrapper').length).toBe(1);
-    });
-  });
   describe('isLoading set to true', () => {
+    const wrapper = shallow(<Dashboard />);
     beforeEach(() => {
-      const wrapper = shallow(<Dashboard />);
-      wrapper.setState({
-        isLoading: true,
-      });
+      wrapper.state = {isLoading:true};
     });
     it('should render dashboard page correctly', () => {
-      const wrapper = shallow(<Dashboard />);
-      expect(toJSON(wrapper)).toMatchSnapshot();
+      expect(wrapper).toMatchSnapshot();
     });
     it('should have class of "mid-section-wrapper"', () => {
-      const wrapper = shallow(<Dashboard />);
       expect(wrapper.find('.mid-section-wrapper').length).toBe(1);
     });
-    it('should have two datasets at page load', () => {
-      const wrapper = shallow(<Dashboard />);
-      expect(wrapper.state('tableData').length).toBe(2);
+    it('should render "Loading" on the page', () => {
+      expect(wrapper.find('.mid-section-wrapper').text()).toEqual('Loading...');
     });
-    it('should get chart data at page load', () => {
-
+    it('should have class of "ball-loading"', () => {
+      expect(wrapper.find('.ball-loading').length).toBe(1);
     });
-    // it('should have a scoreboard with zero number of wins at load time', () => {
-    //   const wrapper = shallow(<Dashboard />);
-    //   wrapper.setState({
-    //     isLoading: false
-    //   });
-    //   expect(wrapper.state('wins')).toBe(0);
-    // });
-    // it('should have a scoreboard with zero number of draws at load time', () => {
-    //   const wrapper = shallow(<Dashboard />);
-    //   wrapper.setState({
-    //     isLoading: false
-    //   });
-    //   expect(wrapper.state('draws')).toBe(0);
-    // });
-    // it('should have a scoreboard with zero number of losses at load time', () => {
-    //   const wrapper = shallow(<Dashboard />);
-    //   wrapper.setState({
-    //     isLoading: false
-    //   });
-    //   expect(wrapper.state('losses')).toBe(0);
-    // });
-    // describe('Player', () => {
-    //   it('should return "Computer as player 3" upon clicking Player Computer', () => {
-    //     const wrapper = shallow(<Dashboard />);
-    //     wrapper.setState({
-    //       isLoading: false
-    //     });
-    //     const playerwrap = mount(<Player name={'Computer'} playerNumber={3} />);
-    //     playerwrap.simulate('click');
-    //     expect(playerwrap.text()).toEqual('Computer as player 3');
-    //   });
-    //   it('should return "Kyle Broflovski as player 2" upon clicking Player Kyle Broflovski', () => {
-    //     const wrapper = shallow(<Dashboard />);
-    //     wrapper.setState({
-    //       isLoading: false
-    //     });
-    //     const playerwrap = mount(<Player name={'Kyle Broflovski'} playerNumber={2} />);
-    //     playerwrap.simulate('click');
-    //     expect(playerwrap.text()).toEqual('Kyle Broflovski as player 2');
-    //   });
-    // });
 
-    // describe('HandButton when playing computer or another player', () => {
-    //   it('should return "Rock" when rock hand component clicked', () => {
-    //     const wrapper = shallow(<Dashboard />);
-    //     wrapper.setState({
-    //       isLoading: false,
-    //       playerChoice: 3
-    //     });
-    //     const handwrap = mount(<HandButton choice={'rock'} hand={'Rock'} />);
-    //     handwrap.simulate('click');
-    //     expect(handwrap.text()).toEqual('Rock');
-    //   });
-    //   it('should return "Paper" when paper hand component clicked', () => {
-    //     const wrapper = shallow(<Dashboard />);
-    //     wrapper.setState({
-    //       isLoading: false,
-    //       playerChoice: 3
-    //     });
-    //     const handwrap = mount(<HandButton choice={'paper'} hand={'Paper'} />);
-    //     handwrap.simulate('click');
-    //     expect(handwrap.text()).toEqual('Paper');
-    //   });
-    //   it('should return "Scissors" when scissors hand component clicked', () => {
-    //     const wrapper = shallow(<Dashboard />);
-    //     wrapper.setState({
-    //       isLoading: false,
-    //       playerChoice: 3
-    //     });
-    //     const handwrap = mount(<HandButton choice={'scissors'} hand={'Scissors'} />);
-    //     handwrap.simulate('click');
-    //     expect(handwrap.text()).toEqual('Scissors');
-    //   });
-    // });
   });
+  describe('isLoading set to false', () => {
+    const wrapper = render(<Dashboard />);
+    beforeEach(() => {
+      wrapper.state = {isLoading: false};
+    });
+    it('should render dashboard page correctly', () => {
+      expect(wrapper).toMatchSnapshot();
+    });
+    it('should have class of "mid-section-wrapper loading"', () => {
+      expect(wrapper.hasClass('mid-section-wrapper loading')).toEqual(true);
+    });
+    it('should Not have a class of "ball-loading"', () => {
+      expect(wrapper.hasClass('ball-loading')).toEqual(false);
+    });
+    it('should have an h1 tag of Dashboard data', () => {
+      expect(wrapper.find('h1').text()).toEqual('Dashboard Data');
+    });
+
+  });
+  //   // it('should have a scoreboard with zero number of wins at load time', () => {
+  //   //   const wrapper = shallow(<Dashboard />);
+  //   //   wrapper.setState({
+  //   //     isLoading: false
+  //   //   });
+  //   //   expect(wrapper.state('wins')).toBe(0);
+  //   // });
+  //   // it('should have a scoreboard with zero number of draws at load time', () => {
+  //   //   const wrapper = shallow(<Dashboard />);
+  //   //   wrapper.setState({
+  //   //     isLoading: false
+  //   //   });
+  //   //   expect(wrapper.state('draws')).toBe(0);
+  //   // });
+  //   // it('should have a scoreboard with zero number of losses at load time', () => {
+  //   //   const wrapper = shallow(<Dashboard />);
+  //   //   wrapper.setState({
+  //   //     isLoading: false
+  //   //   });
+  //   //   expect(wrapper.state('losses')).toBe(0);
+  //   // });
+  //   // describe('Player', () => {
+  //   //   it('should return "Computer as player 3" upon clicking Player Computer', () => {
+  //   //     const wrapper = shallow(<Dashboard />);
+  //   //     wrapper.setState({
+  //   //       isLoading: false
+  //   //     });
+  //   //     const playerwrap = mount(<Player name={'Computer'} playerNumber={3} />);
+  //   //     playerwrap.simulate('click');
+  //   //     expect(playerwrap.text()).toEqual('Computer as player 3');
+  //   //   });
+  //   //   it('should return "Kyle Broflovski as player 2" upon clicking Player Kyle Broflovski', () => {
+  //   //     const wrapper = shallow(<Dashboard />);
+  //   //     wrapper.setState({
+  //   //       isLoading: false
+  //   //     });
+  //   //     const playerwrap = mount(<Player name={'Kyle Broflovski'} playerNumber={2} />);
+  //   //     playerwrap.simulate('click');
+  //   //     expect(playerwrap.text()).toEqual('Kyle Broflovski as player 2');
+  //   //   });
+  //   // });
+
+  //   // describe('HandButton when playing computer or another player', () => {
+  //   //   it('should return "Rock" when rock hand component clicked', () => {
+  //   //     const wrapper = shallow(<Dashboard />);
+  //   //     wrapper.setState({
+  //   //       isLoading: false,
+  //   //       playerChoice: 3
+  //   //     });
+  //   //     const handwrap = mount(<HandButton choice={'rock'} hand={'Rock'} />);
+  //   //     handwrap.simulate('click');
+  //   //     expect(handwrap.text()).toEqual('Rock');
+  //   //   });
+  //   //   it('should return "Paper" when paper hand component clicked', () => {
+  //   //     const wrapper = shallow(<Dashboard />);
+  //   //     wrapper.setState({
+  //   //       isLoading: false,
+  //   //       playerChoice: 3
+  //   //     });
+  //   //     const handwrap = mount(<HandButton choice={'paper'} hand={'Paper'} />);
+  //   //     handwrap.simulate('click');
+  //   //     expect(handwrap.text()).toEqual('Paper');
+  //   //   });
+  //   //   it('should return "Scissors" when scissors hand component clicked', () => {
+  //   //     const wrapper = shallow(<Dashboard />);
+  //   //     wrapper.setState({
+  //   //       isLoading: false,
+  //   //       playerChoice: 3
+  //   //     });
+  //   //     const handwrap = mount(<HandButton choice={'scissors'} hand={'Scissors'} />);
+  //   //     handwrap.simulate('click');
+  //   //     expect(handwrap.text()).toEqual('Scissors');
+  //   //   });
+  //   // });
+  // });
+
 });
